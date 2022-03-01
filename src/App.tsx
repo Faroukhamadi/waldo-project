65952845d0911eaa3c2f171e358d4ba0630f442b
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-03-01 03:19:40 +0100

    get data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
-import picture from './images/beach-waldo.jpg';
+import { db } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 
 const App = () => {
   const [xCoordinate, setXCoordinate] = useState(0);
   const [yCoordinate, setYCoordinate] = useState(0);
+  const waldoCollectionRef = collection(db, 'waldo');
+
+  useEffect(() => {
+    const getWaldoData = async () => {
+      const waldoData = await getDocs(waldoCollectionRef);
+      waldoData.docs.forEach((doc) => {
+        console.log(doc.data());
+      });
+    };
+
+    getWaldoData();
+  }, []);
 
   return (
     <div className="app">
       <Header />
-      <Main xCoordinate={yCoordinate} yCoordinate={yCoordinate} />
+      <Main
+        xCoordinate={xCoordinate}
+        yCoordinate={yCoordinate}
+        setXCoordinate={setXCoordinate}
+        setYCoordinate={setYCoordinate}
+      />
     </div>
   );
 };
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,14 +4,24 @@ import waldoBeach from '../images/beach-waldo.jpg';
 type Props = {
   xCoordinate: string | number;
   yCoordinate: string | number;
+  setXCoordinate: React.Dispatch<React.SetStateAction<number>>;
+  setYCoordinate: React.Dispatch<React.SetStateAction<number>>;
 };
 
+type event = React.MouseEvent<HTMLImageElement, MouseEvent>;
+
 const Main = (props: Props) => {
-  console.log(props.xCoordinate);
+  const handleCoordinates = (e: event) => {
+    let currentTargetRect: DOMRect = e.currentTarget.getBoundingClientRect();
+    const event_offsetX: number = e.pageX - currentTargetRect.left,
+      event_offsetY: number = e.pageY - currentTargetRect.top;
+    console.log(event_offsetX);
+    console.log(event_offsetY);
+  };
 
   return (
     <div className="main">
-      <img src={waldoBeach} alt="" />
+      <img src={waldoBeach} alt="waldo" onClick={handleCoordinates} />
     </div>
   );
 };
